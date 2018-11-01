d2b045b9eed728122b4fbb18fbdd921a06eb0a59
Jerry Hwang <jerryhwang@outlook.com>
2018-11-01 15:33:46 -0700

    bunch of updates

---
+++ function-condition.js
@@ -0,0 +1,8 @@
+//[statement] ? [code when true] : [code when false]
+function myFunction() {
+    var status;
+    var age = 19;
+    status = age > 18 ? "adult" : "minor";
+    console.log(status);
+}
+myFunction()
