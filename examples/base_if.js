35a7c95a87ab5f335ecef273f575e02eaa7aba8b
Jerry Hwang <jerryhwang@outlook.com>
2018-11-09 09:25:44 -0800

    daily updates

--- examples/base_if.js
+++ examples/base_if.js
@@ -1,4 +1,15 @@
+/*
+This file may have multiple example of short js
+*/
+
 var answer = "yes";
 if (answer == "yes") {
     alert(answer)
+}
+
+var isMarried = true;
+if (isMarried) {  // it is not necessary to state (isMarried = true)
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon');
 }
